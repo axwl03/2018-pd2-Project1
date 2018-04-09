8e93c1a095ae8649b2909495e2aa5d97f2530d53
F64056172 <f64056172@mail.ncku.edu.tw>
2018-04-09 22:43:49 +0800

    change C and 6 priority

--- proj_class.cpp
+++ proj_class.cpp
@@ -82,8 +82,8 @@ void organizer::Summon(void) {
 			}
 		}
 	}
-	for(i = 0; i < 4; i++) {//summon C if on the deck
-        if(card[i] == 'C' && FRIEND[0][3] != 0) {
+/*	for(i = 0; i < 4; i++) {//summon C if on the deck
+        if(card[i] == 'C' && FRIEND[0][2] != 0) {
 		     if(MANA < 7)
 			    return;
 			 else {
@@ -92,7 +92,7 @@ void organizer::Summon(void) {
 				MANA -= 7;
 			}
 		}
-	}
+	}*/
 	for(i = 0; i < 4; i++) {//summon 6 if on the deck
 		if(card[i] == '6' &&FRIEND[0][2] != 0) {
 			if(MANA < 4)
@@ -101,6 +101,17 @@ void organizer::Summon(void) {
 				cout << "1 6 ";
 				position();
 				MANA -= 4;
+			}
+		}
+	}
+	for(i = 0; i < 4; i++) {//summon C if on the deck
+		if(card[i] == 'C' && FRIEND[0][2] != 0) {
+			if(MANA < 7)
+				return;
+			else {
+				cout << "1 C ";
+				position();
+				MANA -= 7;
 			}
 		}
 	}
